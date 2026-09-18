68c66fec82ea946ac4287212a7b638ef3ba030f4
Muhammad Amjad <229785857+AmjadTheAnalyst@users.noreply.github.com>
2026-09-18 11:43:23 +0200

    feat:finding duplicate numbers using function

---
+++ Sep_2026/18sep2026_challenge14-18.py
@@ -0,0 +1,74 @@
+# ============================================================
+# PYTHON CODING CHALLENGE 14
+# Find Duplicate Numbers
+# ============================================================
+
+# GOAL:
+# Write a Python program that asks the user to enter 8 integers.
+#
+# The program should find which numbers appear more than once.
+#
+# Finally, display the duplicate numbers.
+
+numbers = '634577' #input("Please enter 8 numbers: ")
+'''numbers_list = []
+for number in numbers:    
+    numbers_list.append(number)
+print(numbers_list)'''
+duplicated = set()
+non_duplicate = set()
+for number in numbers:
+    if numbers.count(number) > 1:
+        duplicated.add(number)
+if len(duplicated) == 0:
+    print("No duplicate found")
+else:
+    print(f'duplicated: {duplicated}')
+
+
+#...using function 
+numbers = '67345789' #input("Please enter 8 numbers: ")
+numbers_list = []
+for number in numbers:    
+    numbers_list.append(int(number))
+#print(numbers_list) [6, 7, 3, 4, 5, 7, 5, 7]
+def find_duplicates(numbers_list):
+    duplicate = set()
+    non_duplicate = set()
+    for number in numbers_list:
+        if numbers_list.count(number) > 1:
+            duplicate.add(number)
+        else:
+            non_duplicate.add(number)
+    return duplicate
+returned_duplicated = find_duplicates(numbers_list)
+if len(returned_duplicated) == 0:
+    print("No duplicates")
+else:
+    print(returned_duplicated)
+
+# ------------------------------------------------------------
+# REQUIREMENTS:
+# ------------------------------------------------------------
+
+# 1. Store the 8 numbers in a list.
+
+# 2. Create a function called find_duplicates.
+
+# 3. The function should receive the list as a parameter.
+
+# 4. Use a set to keep track of numbers you have already seen.
+
+# 5. Use another set to keep track of duplicate numbers.
+
+# 6. Use a for loop.
+
+# 7. The function should return the duplicate numbers.
+
+# 8. Do not use a dictionary.
+
+# 9. Do not use collections.Counter.
+
+# 10. Add at least 2 meaningful comments.
+
+# 11. Do not use external libraries.
